ed5ff6cf0238ea3574ec8ad68166f7ddc6f22aad
dejvis06 <dejvisbeqiraj06@gmail.com>
2022-02-14 21:55:52 +0100

    - connected to localhost db; - crud;

---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,16 @@
+package com.example.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("user")
+@Data
+@Builder
+public class User {
+    @Id
+    private ObjectId id;
+    private String username;
+}
--- src/test/java/com/example/SpringCloudApplicationTests.java
+++ src/test/java/com/example/SpringCloudApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example;
 
+import com.example.entity.User;
+import com.example.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class SpringCloudApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    void contextLoads() {
+        // userRepository.save(User.builder().username("dejvis06").build());
+
+        User user = userRepository.findByUsername("dejvis07");
+        System.err.println(user.toString());
+
+        Optional<User> optionalUser = userRepository.findById("620abc629a0a901c91f6bcd4");
+        System.err.println(optionalUser.get().toString());
+
+        user.setUsername("dejvis07");
+        userRepository.save(user);
+
+        userRepository.deleteById("620abc629a0a901c91f6bcd4");
+    }
 
 }
