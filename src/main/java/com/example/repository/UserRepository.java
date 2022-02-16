d1622ab8cab8b4efa701ab0892eec3fd51285d2c
dbeqiraj <dejvisbeqiraj06@gmail.com>
2022-02-16 12:06:11 +0100

    Removed unnecessary annotation;

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -4,8 +4,6 @@ import com.example.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
-
-   User findByUsername(String username);
+    User findByUsername(String username);
 }
